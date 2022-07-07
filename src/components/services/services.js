be10d01df902620a141f50bbf1fb9ed0321d3f7c
chaurasiapk <chaurasia.pk6@gmail.com>
2022-07-07 13:10:29 +0530

    files formated

--- src/components/services/services.js
+++ src/components/services/services.js
@@ -1,28 +1,24 @@
 import React from "react";
-import "./services.css"
-import image2 from "../../assets/images/image2.png"
+import "./services.css";
+import image2 from "../../assets/images/image2.png";
 
-export const Services = ()=>{
-    return(
-       <div id="services-container" className="services-container">
-            <div className="serviecs">
-                <div className="services-left">
-                    <h1>
-                    Real AR - Real estate virtual solution
-                    </h1>
-                    <p>
-                    Augmation reality solution with latest technology enhances real estate.
-                    </p>
-                    <button className="button1">Learn more</button>
-                </div>
+export const Services = () => {
+  return (
+    <div id="services-container" className="services-container">
+      <div className="serviecs">
+        <div className="services-left">
+          <h1>Real AR - Real estate virtual solution</h1>
+          <p>
+            Augmation reality solution with latest technology enhances real
+            estate.
+          </p>
+          <button className="button1">Learn more</button>
+        </div>
 
-                <div className="services-right">
-                    <img src={image2} atl="image2"/>
-
-                </div>
-
-            </div>
-        
-       </div>
-    );
-}
+        <div className="services-right">
+          <img src={image2} atl="image2" />
+        </div>
+      </div>
+    </div>
+  );
+};
